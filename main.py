a9ee734aa276b201e88e5db1dd9519fa015b875e
lorenabc <71780826+lorenabc@users.noreply.github.com>
2020-10-16 17:27:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ fichas = 100
 
 print(f'Você começará o jogo com {fichas} fichas. Boa sorte')
 
-quantos_baralhos = int(input("Você quer jogar com quantos baralhos? (1/6/8)"))
-
+#variaveis que guardam o número de baralhos e jogadores na partida
+quantos_baralhos = int(input("Você quer jogar com quantos baralhos? (1/6/8) "))
 quantos_jogadores = int(input("Quantos jogadores vão participar? "))
 
 game_on = True
