828eb65afc400261d0420712af71f3c2ec517d97
Paulofalcao2002 <72179636+Paulofalcao2002@users.noreply.github.com>
2020-10-15 23:14:58 -0300

    Update somas.py

--- somas.py
+++ somas.py
@@ -9,3 +9,14 @@ def retorna_valor_soma(a, b, c = 0):
     else:
         return (a + b + c)
 
+#função que confere a necessidade da terceira carta e retorna as strings referentes a soma das mesmas
+def confere_terceira_carta(a, b, c, d, e):
+    if retorna_valor_soma(a, b) <= 5:
+        carta3, valor3 = random.choice(list(baralho.items()))
+        soma = retorna_valor_soma(a, b, valor3)
+        return f'A soma das cartas do {e} foi de {soma}. ' \
+               f'Suas cartas foram {c}, {d} e {carta3}', soma
+    else:
+        soma = retorna_valor_soma(a, b)
+        return f'A soma das cartas do {e} foi de {soma}. ' \
+               f'Suas cartas foram {c} e {d}', soma
