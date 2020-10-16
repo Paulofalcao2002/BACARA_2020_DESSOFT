05a6b4fed4fe176ba63f42a61044a72b7ca4d529
lorenabc <71780826+lorenabc@users.noreply.github.com>
2020-10-15 23:03:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ print(f'Você começará o jogo com {fichas} fichas. Boa sorte')
 quantos_baralhos = int(input("Você quer jogar com quantos baralhos? (1/6/8)"))
 
 quantos_jogadores = int(input("Quantos jogadores vão participar? "))
+
+game_on = True
+
+while game_on:
+    quem_aposta = input('Em quem você deseja apostar? (jogador/banco/empate)')
+    fichas_apostadas = int(input("Quantas fichas você deseja apostar? "))
+
+
+    carta1_jogador, valor1_jogador = sorteia_carta(quantos_baralhos, baralho)
+    carta2_jogador, valor2_jogador = sorteia_carta(quantos_baralhos, baralho)
+    carta1_banco, valor1_banco = sorteia_carta(quantos_baralhos, baralho)
+    carta2_banco, valor2_banco = sorteia_carta(quantos_baralhos, baralho)
