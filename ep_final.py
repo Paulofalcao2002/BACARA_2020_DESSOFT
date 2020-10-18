5239f2a39566575741df7c2e54218894d143f270
lorenabc <71780826+lorenabc@users.noreply.github.com>
2020-10-18 10:48:42 -0300

    Update ep_final.py

--- ep_final.py
+++ ep_final.py
@@ -193,29 +193,38 @@ while game_on:
             else:
                 lista_fichas_apostadas.append(fichas_apostadas)
                 break
-#Sorteando as cartas do jogador e banco
-carta1_jogador, valor1_jogador = sorteia_carta(quantos_baralhos, baralho)
-carta2_jogador, valor2_jogador = sorteia_carta(quantos_baralhos, baralho)
-carta1_banco, valor1_banco = sorteia_carta(quantos_baralhos, baralho)
-carta2_banco, valor2_banco = sorteia_carta(quantos_baralhos, baralho)
+    #Sorteando as cartas do jogador e banco
+    carta1_jogador, valor1_jogador = sorteia_carta(quantos_baralhos, baralho)
+    carta2_jogador, valor2_jogador = sorteia_carta(quantos_baralhos, baralho)
+    carta1_banco, valor1_banco = sorteia_carta(quantos_baralhos, baralho)
+    carta2_banco, valor2_banco = sorteia_carta(quantos_baralhos, baralho)
 
-#Utilizando a função que devolve a soma das cartas, adcionando ou não a terceira carta
-texto_jogador, soma_jogador, jogador_recebeu, valor3 = confere_terceira_carta_jogador(valor1_jogador, valor2_jogador, carta1_jogador, carta2_jogador, 'jogador', valor1_banco, valor2_banco)
-texto_banco, soma_banco = confere_terceira_carta_banco(valor1_banco, valor2_banco, carta1_banco, carta2_banco, 'banco', jogador_recebeu, valor3)
+    #Utilizando a função que devolve a soma das cartas, adcionando ou não a terceira carta
+    texto_jogador, soma_jogador, jogador_recebeu, valor3 = confere_terceira_carta_jogador(valor1_jogador, valor2_jogador, carta1_jogador, carta2_jogador, 'jogador', valor1_banco, valor2_banco)
+    texto_banco, soma_banco = confere_terceira_carta_banco(valor1_banco, valor2_banco, carta1_banco, carta2_banco, 'banco', jogador_recebeu, valor3)
 
-print(texto_jogador)
-print(texto_banco)
+    print(texto_jogador)
+    print(texto_banco)
 
-#Aplicando os ifs e atualizando as fichas dos jogadores depois das apostas
-for i in range(quantos_jogadores):
-    if lista_quem_aposta[i] == 'jogador': lista_fichas_jogadores[i] = aposta_jogador(soma_jogador, soma_banco, lista_fichas_jogadores[i], lista_fichas_apostadas[i], quantos_baralhos)
-    if lista_quem_aposta[i] == 'banco': lista_fichas_jogadores[i] = aposta_banco(soma_jogador, soma_banco, lista_fichas_jogadores[i], lista_fichas_apostadas[i], quantos_baralhos)
-    if lista_quem_aposta[i] == 'empate': lista_fichas_jogadores[i] = aposta_empate(soma_jogador, soma_banco, lista_fichas_jogadores[i], lista_fichas_apostadas[i], quantos_baralhos)
+    #Aplicando os ifs e atualizando as fichas dos jogadores depois das apostas
+    for i in range(quantos_jogadores):
+        if lista_quem_aposta[i] == 'jogador': lista_fichas_jogadores[i] = aposta_jogador(soma_jogador, soma_banco, lista_fichas_jogadores[i], lista_fichas_apostadas[i], quantos_baralhos)
+        if lista_quem_aposta[i] == 'banco': lista_fichas_jogadores[i] = aposta_banco(soma_jogador, soma_banco, lista_fichas_jogadores[i], lista_fichas_apostadas[i], quantos_baralhos)
+        if lista_quem_aposta[i] == 'empate': lista_fichas_jogadores[i] = aposta_empate(soma_jogador, soma_banco, lista_fichas_jogadores[i], lista_fichas_apostadas[i], quantos_baralhos)
 
 
-#Atualizando as variaveis globais dos jogadores após o resultado da rodada
-quantos_jogadores, numero_jogadores, lista_fichas_jogadores, string_eliminados = atualiza_fichas(quantos_jogadores, numero_jogadores, lista_fichas_jogadores)
+    #Atualizando as variaveis globais dos jogadores após o resultado da rodada
+    quantos_jogadores, numero_jogadores, lista_fichas_jogadores, string_eliminados = atualiza_fichas(quantos_jogadores, numero_jogadores, lista_fichas_jogadores)
 
-#Imprimindo as fichas para os jogadores
-for i in range(quantos_jogadores):
-    print(f'Jogador {numero_jogadores[i]}, você tem {lista_fichas_jogadores[i]} fichas ao final da rodada!')
+    #Imprimindo as fichas para os jogadores
+    for i in range(quantos_jogadores):
+        print(f'Jogador {numero_jogadores[i]}, você tem {lista_fichas_jogadores[i]} fichas ao final da rodada!')
+        
+    #Códigos que terminam o loop da partida, todos jogadores zerarem ou desistirem de jogar
+    if quantos_jogadores == 0:
+        print('Todos jogadores zeraram suas fichas')
+        break
+
+    continua = input("Quer continuar jogando? (sim/não) ")
+    if continua == 'não': break
+
