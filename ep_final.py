d3c82c84ce1d359d2e10573a931096b6470ee6e2
Paulofalcao2002 <72179636+Paulofalcao2002@users.noreply.github.com>
2020-10-18 10:27:58 -0300

    Update ep_final.py

--- ep_final.py
+++ ep_final.py
@@ -105,4 +105,19 @@ def retona_comissao(quantos_baralhos):
         comissao_banco = 0.0106
         comissao_empate = 0.1444
         return comissao_jogador, comissao_banco, comissao_empate
+      
+    if quantos_baralhos == 8:
+        comissao_jogador = 0.0124
+        comissao_banco = 0.0106
+        comissao_empate = 0.1436
+        return comissao_jogador, comissao_banco, comissao_empate
+
+#funções que atualizam o número das fichas de acordo com cada tipo de aposta
+def aposta_jogador(soma_jogador, soma_banco, fichas, fichas_apostadas, quantos_baralhos):
+    if soma_jogador > soma_banco:
+        fichas += fichas_apostadas - (retona_comissao(quantos_baralhos)[0] * fichas_apostadas)
+        return int(fichas)
+    else:
+        fichas -= fichas_apostadas
+        return fichas
 
