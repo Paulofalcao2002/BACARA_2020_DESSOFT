c53600c36edd3ff876571ccc7ec4d68fdd9feba2
lorenabc <71780826+lorenabc@users.noreply.github.com>
2020-10-18 10:26:44 -0300

    Update ep_final.py

--- ep_final.py
+++ ep_final.py
@@ -92,3 +92,17 @@ else:
      return f'A soma das cartas do {string} foi de {soma}. ' \
             f'Suas cartas foram {carta1_banco} e {carta2_banco}', soma
           
+#função que retorna os valores das comissões de acordo com o número de baralhos
+def retona_comissao(quantos_baralhos):
+    if quantos_baralhos == 1:
+        comissao_jogador = 0.0129
+        comissao_banco = 0.0101
+        comissao_empate = 0.1575
+        return comissao_jogador, comissao_banco, comissao_empate
+
+    if quantos_baralhos == 6:
+        comissao_jogador = 0.0124
+        comissao_banco = 0.0106
+        comissao_empate = 0.1444
+        return comissao_jogador, comissao_banco, comissao_empate
+
